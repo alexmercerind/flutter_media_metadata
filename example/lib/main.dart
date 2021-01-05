014923724a82b7f4760a4785175f1f647f37de09
alexmercerind <saini123hitesh@gmail.com>
2021-01-05 20:08:57 +0530

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,24 +19,29 @@ class _MyAppState extends State<MyApp> {
   Widget mediaAlbumArt;
   Widget mediaMetadata;
 
-  // Method called after pressing FAB
+  // Method called by FAB
   Future<void> onSelected(String mediaFilePath) async {
     FocusScope.of(context).unfocus();
-    // Instantiating MediaMetadataRetriever class
+    // Instantiate MediaMetadataRetriever
     var metadataRetriever = new MediaMetadataRetriever();
-    // Setting path of media file
+    // Set file path
     await metadataRetriever.setFile(File(mediaFilePath));
-    // Getting Metadata of the media file
+    // Get metadata
     this.metadata = await metadataRetriever.metadata;
-
     this.setState(() {
-      this.mediaAlbumArt = new Image.memory(
-        // Getting album art of media file as Uint8List
-        metadataRetriever.albumArt,
-        height: 200.0,
-        width: 200.0,
-      );
-      // Displaying metadata in a DataTable
+      this.mediaAlbumArt = metadataRetriever.albumArt != null
+          ? new Image.memory(
+              // Get album art
+              metadataRetriever.albumArt,
+              height: 200.0,
+              width: 200.0,
+            )
+          : Container(
+              height: 200.0,
+              width: 200.0,
+              child: Text('No Album Art'),
+            );
+      // Display metadata in a DataTable
       this.mediaMetadata = new SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: DataTable(
@@ -51,92 +56,86 @@ class _MyAppState extends State<MyApp> {
           rows: [
             DataRow(
               cells: [
-                DataCell(Text('title')),
-                DataCell(Text(this.metadata.title)),
+                DataCell(Text('trackName')),
+                DataCell(Text('${this.metadata.trackName}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('album')),
-                DataCell(Text(this.metadata.album)),
+                DataCell(Text('trackArtistNames')),
+                DataCell(Text('${this.metadata.trackArtistNames}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('artists')),
-                DataCell(Text(this.metadata.artists)),
+                DataCell(Text('albumName')),
+                DataCell(Text('${this.metadata.albumName}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('year')),
-                DataCell(Text(this.metadata.year)),
+                DataCell(Text('albumArtistName')),
+                DataCell(Text('${this.metadata.albumArtistName}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('trackNumber')),
-                DataCell(Text(this.metadata.trackNumber)),
+                DataCell(Text('${this.metadata.trackNumber}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('albumLength')),
-                DataCell(Text(this.metadata.albumLength)),
+                DataCell(Text('${this.metadata.albumLength}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('albumArtist')),
-                DataCell(Text(this.metadata.albumArtist)),
+                DataCell(Text('year')),
+                DataCell(Text('${this.metadata.year}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('genre')),
-                DataCell(Text(this.metadata.genre)),
+                DataCell(Text('${this.metadata.genre}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('author')),
-                DataCell(Text(this.metadata.author)),
+                DataCell(Text('authorName')),
+                DataCell(Text('${this.metadata.authorName}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('writer')),
-                DataCell(Text(this.metadata.writer)),
+                DataCell(Text('writerName')),
+                DataCell(Text('${this.metadata.writerName}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('discNumber')),
-                DataCell(Text(this.metadata.discNumber)),
-              ],
-            ),
-            DataRow(
-              cells: [
-                DataCell(Text('filePath')),
-                DataCell(Text(this.metadata.filePath)),
+                DataCell(Text('${this.metadata.discNumber}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('mimeType')),
-                DataCell(Text(this.metadata.mimeType)),
+                DataCell(Text('${this.metadata.mimeType}')),
               ],
             ),
             DataRow(
               cells: [
-                DataCell(Text('duration')),
-                DataCell(Text(metadataRetriever.duration.toString())),
+                DataCell(Text('trackDuration')),
+                DataCell(Text('${this.metadata.trackDuration}')),
               ],
             ),
             DataRow(
               cells: [
                 DataCell(Text('bitrate')),
-                DataCell(Text(metadataRetriever.bitrate.toString())),
+                DataCell(Text('${this.metadata.bitrate}')),
               ],
             ),
           ],
