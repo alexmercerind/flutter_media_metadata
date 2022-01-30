9ce4f4ba4fbb4ec9b5f26a744fa039dfb24ef5c6
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-01-30 21:26:52 +0530

    Plugin: Add web support & improve structure

--- lib/flutter_media_metadata.dart
+++ lib/flutter_media_metadata.dart
@@ -1,121 +1,32 @@
-import 'dart:io';
-import 'dart:convert';
-import 'dart:typed_data';
-import 'package:flutter/services.dart';
+/// ## flutter_media_metadata
+///
+/// A Flutter plugin to read metadata of media files.
+///
+/// MIT License.
+/// Copyright (c) 2021-2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+///
+/// _Minimal Example_
+/// ```dart
+/// final metadata = MetadataRetriever.fromBytes(byteData);
+/// String? trackName = metadata.trackName;
+/// List<String>? trackArtistNames = metadata.trackArtistNames;
+/// String? albumName = metadata.albumName;
+/// String? albumArtistName = metadata.albumArtistName;
+/// int? trackNumber = metadata.trackNumber;
+/// int? albumLength = metadata.albumLength;
+/// int? year = metadata.year;
+/// String? genre = metadata.genre;
+/// String? authorName = metadata.authorName;
+/// String? writerName = metadata.writerName;
+/// int? discNumber = metadata.discNumber;
+/// String? mimeType = metadata.mimeType;
+/// int? trackDuration = metadata.trackDuration;
+/// int? bitrate = metadata.bitrate;
+/// Uint8List? albumArt = metadata.albumArt;
+/// ```
+///
+library flutter_media_metadata;
 
-var _kChannel = const MethodChannel('flutter_media_metadata');
-
-class MetadataRetriever {
-  static Future<Metadata> fromFile(
-    File file, {
-    bool createNewInstance: false,
-  }) async {
-    var metadata = await _kChannel.invokeMethod(
-      'MetadataRetriever',
-      {
-        'filePath': file.path,
-        'createNewInstance': createNewInstance,
-      },
-    );
-    metadata['filePath'] = file.path;
-    return Metadata.fromJson(metadata);
-  }
-}
-
-class Metadata {
-  final String? trackName;
-  final List<String>? trackArtistNames;
-  final String? albumName;
-  final String? albumArtistName;
-  final int? trackNumber;
-  final int? albumLength;
-  final int? year;
-  final String? genre;
-  final String? authorName;
-  final String? writerName;
-  final int? discNumber;
-  final String? mimeType;
-  final int? trackDuration;
-  final int? bitrate;
-  final Uint8List? albumArt;
-  final String filePath;
-
-  const Metadata({
-    this.trackName,
-    this.trackArtistNames,
-    this.albumName,
-    this.albumArtistName,
-    this.trackNumber,
-    this.albumLength,
-    this.year,
-    this.genre,
-    this.authorName,
-    this.writerName,
-    this.discNumber,
-    this.mimeType,
-    this.trackDuration,
-    this.bitrate,
-    this.albumArt,
-    required this.filePath,
-  });
-
-  factory Metadata.fromJson(dynamic map) => Metadata(
-        trackName: map['metadata']['trackName'],
-        trackArtistNames: map['metadata']['trackArtistNames'] != null
-            ? map['metadata']['trackArtistNames'].split('/')
-            : null,
-        albumName: map['metadata']['albumName'],
-        albumArtistName: map['metadata']['albumArtistName'],
-        trackNumber: _parse(map['metadata']['trackNumber']),
-        albumLength: _parse(map['metadata']['albumLength']),
-        year: _parse(map['metadata']['year']),
-        genre: map['genre'],
-        authorName: map['metadata']['authorName'],
-        writerName: map['metadata']['writerName'],
-        discNumber: _parse(map['metadata']['discNumber']),
-        mimeType: map['metadata']['mimeType'],
-        trackDuration: _parse(map['metadata']['trackDuration']),
-        bitrate: _parse(map['metadata']['bitrate']),
-        albumArt: map['albumArt'],
-        filePath: map['filePath'],
-      );
-
-  Map<String, dynamic> toJson() => {
-        'trackName': trackName,
-        'trackArtistNames': trackArtistNames,
-        'albumName': albumName,
-        'albumArtistName': albumArtistName,
-        'trackNumber': trackNumber,
-        'albumLength': albumLength,
-        'year': year,
-        'genre': genre,
-        'authorName': authorName,
-        'writerName': writerName,
-        'discNumber': discNumber,
-        'mimeType': mimeType,
-        'trackDuration': trackDuration,
-        'bitrate': bitrate,
-        'filePath': filePath,
-      };
-
-  @override
-  String toString() => JsonEncoder.withIndent('    ').convert(toJson());
-}
-
-int? _parse(dynamic value) {
-  if (value == null) {
-    return null;
-  }
-  if (value is int) {
-    return value;
-  } else if (value is String) {
-    try {
-      try {
-        return int.parse(value);
-      } catch (_) {
-        return int.parse(value.split('/').first);
-      }
-    } catch (_) {}
-  }
-  return null;
-}
+export 'package:flutter_media_metadata/src/flutter_media_metadata_native.dart'
+    if (dart.library.html) 'package:flutter_media_metadata/src/flutter_media_metadata_web.dart';
+export 'package:flutter_media_metadata/src/models/metadata.dart';
